7d809bb90cb397a6730edc58ad162d4d9cf63861
Ihza Ahmad Abror Amrullah <ihzaahmad0@gmail.com>
2023-03-10 14:51:21 +0700

    99 Route Faker

---
+++ faker.js
@@ -0,0 +1,8 @@
+import { connection } from "./connection.js";
+
+connection().then(async () => {
+    const args = process.argv;
+
+    const fakerFile = args[2];
+    const faker = await import(`./${fakerFile}`);
+});
